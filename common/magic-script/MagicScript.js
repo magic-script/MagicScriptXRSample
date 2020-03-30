91694d03053fe8d171a7f0a38124c212c436c449
Tony Cuadra <tcuadra@magicleap.com>
2020-03-30 09:41:30 -0400

    Add Lumin support to XR Sample (#3)
    * Update XR Sample for new prism-level anchoring
    
    * Re-init app with lumin support
    
    * Add platform-dependent oAuth hooks
    
    * Remove duplicate magic-script-components dependency
    
    This was causing issues at runtime. Specifically, the xrClient instance
    set on XrClientProvider was undefined (because there was more than one
    instance of XrClientProvider).
    
    * Update unpublished deps to master branch
    
    * Git ignore android bundle

--- common/magic-script/MagicScript.js
+++ common/magic-script/MagicScript.js
@@ -1,6 +1,13 @@
-import { AppRegistry } from 'react-native';
+import { AppRegistry, NativeModules } from 'react-native';
+import { XrClientProvider } from 'magic-script-components';
 import { ReactNativeMagicScript } from 'magic-script-components-react-native';
 import ReactNativeApp from '../react-native/ReactNativeApp';
+import { authorize } from 'react-native-app-auth';
+
+XrClientProvider.setXrClient(NativeModules.XrClientBridge);
+
+// Tack on authorize function for react-native only (not needed for lumin)
+XrClientProvider.authorize = authorize;
 
 const MagicScript = {
     registerApp: (name, appComponent, debug = false) => {
